75147dbbee20ef300e70047fdba05b1d87cbebae
zzwlovelife <zzwlovewxq@gmail.com>
2019-08-20 21:17:33 +0800

    opencv_test.py增加功能

--- opencv_test.py
+++ opencv_test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 #打开摄像头，每3秒取一帧，并显示，以数字保存在/mnt/hgfs/vmware_shared/
+#如果满20张，则停止
 import cv2
 capture = cv2.VideoCapture(0)
 num=1
@@ -13,5 +14,7 @@ while(True):
     pathname='/mnt/hgfs/vmware_shared/'+str(num)+'.jpg'
     cv2.imwrite(pathname,frame)
     num=num+1
+    if(num==21):
+        break
     if cv2.waitKey(3000) == ord('q'):
         break
